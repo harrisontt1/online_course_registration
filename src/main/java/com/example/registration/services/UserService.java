9b0018479338dafbdb9ab38259882aa8b4527a89
mmandyck88 <mealva20@asu.edu>
2026-07-22 01:42:17 +0200

    Update UserService.java
    Modified:
    getRole(username, password)

--- src/main/java/com/example/registration/services/UserService.java
+++ src/main/java/com/example/registration/services/UserService.java
@@ -6,13 +6,17 @@ import com.example.registration.dao.UserDAO;
 @Service
 public class UserService {
 
-	private final UserDAO userDAO;
+    private final UserDAO userDAO;
 
-	public UserService(UserDAO userDAO) {
-		this.userDAO = userDAO;
-	}
+    public UserService(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
 
-	public boolean authenticate(String username, String password) {
-		return userDAO.authenticate(username, password);
-	}
+    public boolean authenticate(String username, String password) {
+        return userDAO.authenticate(username, password);
+    }
+
+    public String getRole(String username, String password) {
+        return userDAO.getRole(username, password);
+    }
 }
